ca08c902d8e9dbe84d5f887e32404da601bc40f2
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2021-12-29 18:58:23 +0100

    Create husky, lint-staged and sentry configs

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: `https://${process.env.REACT_APP_SENTRY_PUBLIC_KEY}.ingest.sentry.io/6128012`,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
