380877f42238ba7daee9daf767876b127e3a54e9
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2022-01-05 16:29:55 +0100

    Install, configure i18n and create basic translations

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,28 @@
 import { GlobalStyle } from './lib/styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './lib/styles/theme';
+import './lib/i18n';
+import { useTranslation } from 'react-i18next';
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <h1>Hello world</h1>
-  </ThemeProvider>
-);
+const App = () => {
+  const { t, i18n } = useTranslation('translation', { useSuspense: false });
+
+  const changeLng = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    i18n.changeLanguage(event.currentTarget.value, (err, t) => {
+      if (err) return console.log('something went wrong loading', err);
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <h1>{t('title')}</h1>
+      <select onChange={changeLng} value={i18n.language}>
+        <option value="pl-PL">PL</option>
+        <option value="en">EN</option>
+      </select>
+    </ThemeProvider>
+  );
+};
 
 export default App;
